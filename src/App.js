041c8bd4b31e31b1bf376dd113214ce3f42ac0aa
Simeon Vibbi <svibbinyakeh@gmail.com>
2024-09-07 05:03:39 -0700

    React Tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+// // Create a component..
+
+// function Greetings() {
+//   return <p>Hello there! Good morning!</p>;
+// }
+
+// // Avatar component...
+// function Avatar() {
+//   return (
+//     <img
+//       className="card-image"
+//       src="https://images.unsplash.com/photo-1706885093487-7eda37b48a60?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+//       alt="Avatar"
+//       style={{ width: 100 }}
+//     />
+//   );
+// }
+
+// // Profile card....
+// function UserProfileCard() {
+//   return (
+//     <div className="card">
+//       <Avatar />
+//       <div className="card-name">Josephine Kargbo</div>
+//       <div>Josephine Kargbo is a diligent relator agent</div>
+//     </div>
+//   );
+// }
+
+// // A list item component...
+// function ListItem() {
+//   return <li>Tesla</li>;
+// }
+
+// // Create our root component => App component
+// const App = () => {
+
+//   // List of items..
+//   const items = ['Tesla', 'Audi', 'BMW', 'Mercedes', 'Volkswagon'];
+
+//   // Create a list of list items..
+//   const ListItems = items.map(item => {
+//     return <li key={item}>{item}</li>
+//   })
+
+//   return (
+//     <ol>
+//       { ListItems}
+//     </ol>
+//   );
+// };
 
 function App() {
+  // State...
+  const [counter, setCounter] = useState(0);
+
+  // Handle increment...
+  const handleIncrement = (event) => {
+    event.preventDefault();
+    setCounter((prev) => prev + 1);
+  };
+
+  // Handle decrement...
+  const handleDecrement = (event) => {
+    event.preventDefault();
+    setCounter((prev) => prev - 1);
+  };
+
+  // Render...
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="counter-container">
+      <div className="counter-label">{counter}</div>
+      <button className="action-btn" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button className="action-btn" onClick={handleDecrement}>
+        Decrement
+      </button>
+    </section>
   );
 }
 
